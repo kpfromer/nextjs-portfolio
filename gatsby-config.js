26f876742968a0ba502fded7b1bab4cc05401756
Kyle Pfromer <kpfromer2@gmail.com>
2019-11-25 17:42:05 -0700

    Update packages and config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,34 @@
 module.exports = {
   siteMetadata: {
-    title: `Kyle Pfromer`,
+    firstName: 'Kyle',
+    lastName: 'Pfromer',
+    theme: {
+      primary: '#e3872d',
+      secondary: '#041230'
+    }
+    // social: { TODO:
+    //   linkedin
+    //   github
+    //   stackoverflow
+    // }
   },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
-        path: `${__dirname}/src/`,
-      },
+        path: `${__dirname}/src/`
+      }
     },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-  ],
-}
+        pathToConfigModule: `src/utils/typography`
+      }
+    }
+  ]
+};
