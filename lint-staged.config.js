9e9c16ec792362e1ca4fb112eeb2201c2250c757
kpfromer <kpfromer2@gmail.com>
2020-04-06 22:37:31 -0600

    fix: fix lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  './src/**/*.(js|md)': ['prettier --write', 'git add'],
-  './src/**/*.(js)': ['eslint --fix', 'git add']
+  './src/**/*.(js|md)': ['prettier --write'],
+  './src/**/*.(js)': ['eslint --fix']
 };
