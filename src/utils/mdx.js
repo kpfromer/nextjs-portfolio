ab1cb1e091534206900bf9ddd147ed813a43a4ef
Kyle Pfromer <kpfromer2@gmail.com>
2020-04-13 22:15:05 -0600

    feat: resize mdx headers

--- src/utils/mdx.js
+++ src/utils/mdx.js
@@ -1,6 +1,32 @@
 import { Link } from '../components/Link';
+import { Heading } from 'rebass';
+
+const Header = ({ children, as, ...props }) => {
+  let fontSize = 2;
+  if (as === 'h1') {
+    fontSize = 6;
+  } else if (as === 'h2') {
+    fontSize = 5;
+  } else if (as === 'h3') {
+    fontSize = 4;
+  } else if (as === 'h4') {
+    fontSize = 3;
+  }
+
+  return (
+    <Heading {...props} as={as} fontSize={fontSize} my={3}>
+      {children}
+    </Heading>
+  );
+};
 
 export default {
+  h1: (props) => <Header as="h1" {...props} />,
+  h2: (props) => <Header as="h2" {...props} />,
+  h3: (props) => <Header as="h3" {...props} />,
+  h4: (props) => <Header as="h4" {...props} />,
+  h5: (props) => <Header as="h5" {...props} />,
+  h6: (props) => <Header as="h6" {...props} />,
   a: ({ href, children }) => (
     <Link outside={true} to={href}>
       {children}
