64e06e777acd619cdd4ea2a7d17f7d08495cbccc
Kyle Pfromer <kpfromer2@gmail.com>
2021-01-07 14:19:49 -0700

    refactor: add color to social links

--- components/SocialLinks/index.tsx
+++ components/SocialLinks/index.tsx
@@ -1,19 +1,23 @@
 import { FiGithub, FiLinkedin } from 'react-icons/fi';
 import { FaStackOverflow } from 'react-icons/fa';
-import { Box, Icon, HStack, StackProps } from '@chakra-ui/react';
+import { Box, Icon, HStack, StackProps, useColorModeValue } from '@chakra-ui/react';
 
-export interface SocialLinksProps extends Omit<StackProps, 'spacing'> {
+export interface SocialLinksProps extends Omit<StackProps, 'spacing' | 'color'> {
   spacing?: string;
   iconSize?: string;
+  color?: string;
 }
 
 const SocialLinks: React.FC<SocialLinksProps> = ({
   spacing = '15px',
   iconSize = '35px',
+  color,
   ...props
 }) => {
+  const defaultColor = useColorModeValue('gray.700', 'white');
+
   return (
-    <HStack color="white" spacing={spacing} {...props}>
+    <HStack color={color ? color : defaultColor} spacing={spacing} {...props}>
       <Box as="a" target="_blank" rel="noopener noreferrer" href="https://github.com/kpfromer">
         <Icon as={FiGithub} boxSize={iconSize} aria-label="Github" />
       </Box>
