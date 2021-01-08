919a99d2cbe3fa6f76394b6e01223d095807bdc1
Kyle Pfromer <kpfromer2@gmail.com>
2021-01-08 14:07:44 -0700

    fix: 🐛 dark github button hover bg color

--- components/IconButton/index.tsx
+++ components/IconButton/index.tsx
@@ -24,33 +24,41 @@ const Button = forwardRef<HTMLElement, ButtonProps>(
 export interface IconButtonProps extends HTMLAttributes<HTMLElement> {
   as?: string;
   icon?: React.ReactElement;
+  overrideClassName?: boolean;
 }
 
-const IconButton = forwardRef<HTMLElement, IconButtonProps>(({ icon, children, ...props }, ref) => {
-  /**
-   * Passing the icon as prop or children should work
-   */
-  const element = icon || children;
-  const _children = React.isValidElement(element)
-    ? React.cloneElement(element as any, {
-        'aria-hidden': true,
-        focusable: false,
-      })
-    : null;
+const IconButton = forwardRef<HTMLElement, IconButtonProps>(
+  ({ icon, children, overrideClassName = false, ...props }, ref) => {
+    /**
+     * Passing the icon as prop or children should work
+     */
+    const element = icon || children;
+    const _children = React.isValidElement(element)
+      ? React.cloneElement(element as any, {
+          'aria-hidden': true,
+          focusable: false,
+        })
+      : null;
 
-  return (
-    <Button
-      {...props}
-      // @ts-ignore
-      children={_children}
-      // @ts-ignore
-      ref={ref}
-      className={classnames(
-        'rounded-lg inline-block p-3 bg-gray-200 text-black hover:bg-gray-300 dark:text-white dark:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-75 transition-colors ease-in-out',
-        props.className,
-      )}
-    />
-  );
-});
+    return (
+      <Button
+        {...props}
+        // @ts-ignore
+        children={_children}
+        // @ts-ignore
+        ref={ref}
+        overrideClassname
+        className={
+          overrideClassName
+            ? props.className
+            : classnames(
+                'rounded-lg inline-block p-3 bg-gray-200 text-black hover:bg-gray-300 dark:text-white dark:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-75 transition-colors ease-in-out',
+                props.className,
+              )
+        }
+      />
+    );
+  },
+);
 
 export default IconButton;
