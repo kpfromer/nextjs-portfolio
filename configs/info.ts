fd55106ec6761269e594fbcd3ce3941e93095efa
Kyle Pfromer <kpfromer2@gmail.com>
2021-01-08 19:44:47 -0700

    fix: 🐛 base url

--- configs/info.ts
+++ configs/info.ts
@@ -1,5 +1,5 @@
 const info = {
-  baseUrl: 'https://kpfromer.com',
+  baseUrl: 'https://kylepfromer.com',
   resume: 'https://drive.google.com/file/d/1KUxBSQwj87LBoHmsuRAyPgfN6F6oo5kR/view',
   github: 'https://github.com/kpfromer',
   linkedin: 'https://www.linkedin.com/in/kyle-pfromer',
