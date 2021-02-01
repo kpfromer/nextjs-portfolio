66fc058e879ed857d2c7cdad31edc59d31e37440
Kyle Pfromer <kpfromer2@gmail.com>
2021-02-01 10:08:15 -0700

    fix: jest tests

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,9 @@ module.exports = {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
-  // This makes sure that paths defined in compilerOptions.paths in tsconfig.json work as expected
-  moduleNameMapper,
+  moduleNameMapper: {
+    // This makes sure that paths defined in compilerOptions.paths in tsconfig.json work as expected
+    ...moduleNameMapper,
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
 };
