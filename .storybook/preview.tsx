699e8af75803fcf278ee83bafa1ccf1ea4827639
Kyle Pfromer <kpfromer2@gmail.com>
2021-02-08 13:38:12 -0700

    chore: remove storybooks

--- .storybook/preview.tsx
+++
@@ -1,7 +0,0 @@
-import { withNextRouter } from 'storybook-addon-next-router';
-
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-};
-
-export const decorators = [withNextRouter];
