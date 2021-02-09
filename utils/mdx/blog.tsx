d31140e6c15f6dc91cb4dd155605e8a350f5a701
Kyle Pfromer <kpfromer2@gmail.com>
2021-02-08 19:27:12 -0700

    fix: prose img distorting images

--- utils/mdx/blog.tsx
+++ utils/mdx/blog.tsx
@@ -7,7 +7,7 @@ import Img from '@components/Img';
 const mdxComponents: MDXProviderComponentsProp = {
   ...baseMdxComponents,
   img: (props) => (
-    <div className="rounded my-2 overflow-hidden flex">
+    <div className="rounded my-3 overflow-hidden flex">
       <Img
         {...props}
         containerProps={{ className: 'mx-auto' }}
